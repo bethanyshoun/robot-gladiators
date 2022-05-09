76a7973619aa58de054c6ed49beb5d9e8cfef2c9
bethanyshoun <bethanyshoun@gmail.com>
2022-05-08 23:21:51 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,7 +21,7 @@ for(var i = 0; i < enemyNames.length; i++) {
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     // Prompt fight or skip round
@@ -74,3 +74,6 @@ var fight = function() {
 
 
 // fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
